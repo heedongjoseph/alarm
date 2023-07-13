060e7d60c14f4d17af19c74c403bd0e975662b4d
LAPTOP-M80SHDUR\lolo1 <90heedong@gmail.com>
2023-07-14 05:13:55 +0900

    修正

--- obj/Debug/net7.0-windows/MainWindow.g.cs
+++ obj/Debug/net7.0-windows/MainWindow.g.cs
@@ -84,7 +84,7 @@ namespace alarm {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/alarm;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/alarm;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
