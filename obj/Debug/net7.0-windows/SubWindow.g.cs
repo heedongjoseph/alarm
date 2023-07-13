e63c6044426d7c7c62bcd020b21ed823c4df574a
LAPTOP-M80SHDUR\lolo1 <90heedong@gmail.com>
2023-07-14 06:02:49 +0900

    修正

--- obj/Debug/net7.0-windows/SubWindow.g.cs
+++ obj/Debug/net7.0-windows/SubWindow.g.cs
@@ -68,7 +68,7 @@ namespace alarm {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/alarm;component/subwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/alarm;V1.0.0.0;component/subwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SubWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
